52b23309af4b14921e2307309b2f1a08ad12ea59
Dimas Agustiawan <dimasagustiawan30@gmail.com>
2022-11-08 22:22:41 +0700

    Test automation login

---
+++ src/login/Test_Login.java
@@ -0,0 +1,38 @@
+package login;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.interactions.Actions;
+
+public class Test_Login {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "D:\\Firefox\\chromedriver.exe");  
+		
+		WebDriver driver=new ChromeDriver();  
+		
+		 // Launch Website  
+        driver.navigate().to("http://www.facebook.com/");
+        Actions aksi = new Actions(driver);
+        
+        // Masukkan Email
+        WebElement textemail = driver.findElement(By.name("email"));
+        Action tulisEmail = aksi.sendKeys(textemail, "sdas").build();
+        tulisEmail.perform();
+        
+        // Masukkan Password
+        WebElement textpassword = driver.findElement(By.name("pass"));
+        Action tulisPass = aksi.sendKeys(textpassword, "dsd").build();
+        tulisPass.perform();
+        
+        // Klik Button Login
+        WebElement btn = driver.findElement(By.name("login"));
+        Action klikBtn = aksi.click(btn).build();
+        klikBtn.perform();
+	}
+
+}
